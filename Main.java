5fbc728d81a2bd74aa86ce6202333cdceb994c81
Antonio Miloloža <128092036+Mili666999@users.noreply.github.com>
2023-04-12 12:06:16 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,18 +2,30 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner read = new Scanner(System.in);
+        System.out.print("Ime: ");
         String firstName = read.nextLine();
+        System.out.print("Prezime: ");
         String secondName = read.nextLine();
+        System.out.print("Godine: ");
         int age = read.nextInt();
+        System.out.print("Broj sobe: ");
         int roomNumber = read.nextInt();
         Customer customer = new Customer();
         //set customer's data to object here
+        customer.firstName = firstName;
+        customer.secondName = secondName;
+        customer.age = age;
+        customer.roomNumber = roomNumber;
 
         customer.saveCustomerInfo();
     }
 }
 class Customer {
     //add all necessary attributes here
+    String firstName;
+    String secondName;
+    int age;
+    int roomNumber;
 
     public void saveCustomerInfo() {
         System.out.println("First name: " + firstName);
